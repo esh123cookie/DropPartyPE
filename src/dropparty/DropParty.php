2753c82e60a8d274d26899a43be5e8b6c5bd8edd
Netstarv2 <33966611+Netstarv2@users.noreply.github.com>
2018-06-26 09:14:48 +0530

    Update DropParty.php

--- src/dropparty/DropParty.php
+++ src/dropparty/DropParty.php
@@ -3,70 +3,37 @@
 
 
 namespace dropparty;
-
-
-
 use pocketmine\plugin\PluginBase;
-
 use dropparty\task\DropItemsTask;
-
 use dropparty\task\DropPartyTask;
-
 use pocketmine\utils\Config;
-
-
 
 class DropParty extends PluginBase {
 
-	
-
 	public $secs = 0;
-
-	public $tasks = [];
-
-	public $status;
-
-	public $time;
-
-	
+        public $tasks = [];
+        public $status;
+        public $time;
 
 	public function onEnable() {
-
-		$this->getLogger()->info("DropParty by Netstarv2 has been enabled");
-
-		@mkdir($this->getDataFolder());
-
-		@mkdir($this->getDataFolder() . "config/");
-
-		$this->cfg = (new Config($this->getDataFolder() . "config/" . "config.yml", Config::YAML, array(
-
-		"World" => "world",
-
-		"Time" => 10,
-
-		"Duration" => 60,
-
-		"Message.Started" => "§a[§6Drop§bParty§a] §dHas started! Do /spawn to get the items at the §5DropParty",
-
-		"Message.Ended" => "§a[§6Drop§bParty§a] §dHas ended! .",
-
-		"Message.Countdown" => "§a[§6Drop§bParty§a] §dis starting in §5{time} §dminutes.",
-
-		"Popup.Enabled" => true,
-
-		"Popup.Message" => "§a[§6Drop§bParty§a] §dItems are dropping! §5Do /spawn to get those items!",
-
-		"Coordinates" => [
-
-		"X" => 0,
-
-		"Y" => 0,
-
-		"Z" => 0,
-
-		],
-
-		"Items" => [
+                $this->getLogger()->info("DropParty by Netstarv2 has been enabled");
+                @mkdir($this->getDataFolder());
+                @mkdir($this->getDataFolder() . "config/");
+                $this->cfg = (new Config($this->getDataFolder() . "config/" . "config.yml", Config::YAML, array(
+                "World" => "world",
+                "Time" => 10,
+                "Duration" => 60,
+                "Message.Started" => "§a[§6Drop§bParty§a] §dHas started! Do /spawn to get the items at the §5DropParty",
+                "Message.Ended" => "§a[§6Drop§bParty§a] §dHas ended! .",
+                "Message.Countdown" => "§a[§6Drop§bParty§a] §dis starting in §5{time} §dminutes.",
+                "Popup.Enabled" => true,
+                "Popup.Message" => "§a[§6Drop§bParty§a] §dItems are dropping! §5Do /spawn to get those items!",
+                "Coordinates" => [
+                "X" => 0,
+                "Y" => 0,
+                "Z" => 0,
+                 ],
+              "Items" => [
 
 		57,
 
